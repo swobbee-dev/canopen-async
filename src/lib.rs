bf3ec929df4c355e076b0ca04a36f3876fbc564b
Malte Brieske <m.brieske@greenpack.de>
2025-09-03 14:36:01 +0200

    implement sdo block download

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #![no_std]
 
 use core::cell::RefCell;
+use crc::{Algorithm, Crc};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex, signal::Signal};
 use embassy_time::Duration;
 use embedded_can::{Frame, Id, StandardId, asynch::CanTx};
+
+const ABORT_INVALID_BLOCK_SIZE: u32 = 0x05040002;
 
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
@@ -45,6 +48,8 @@ struct RequestState<FRAME, TX: CanTx<Frame = FRAME>> {
     sig_word: Signal<NoopRawMutex, Result<u32, SdoError<TX::Error>>>,
     sig_seg: Signal<NoopRawMutex, Result<Segment, SdoError<TX::Error>>>,
     sig_ack: Signal<NoopRawMutex, Result<(), SdoError<TX::Error>>>,
+    sig_block_init: Signal<NoopRawMutex, Result<BlockInit, SdoError<TX::Error>>>,
+    sig_block_ack: Signal<NoopRawMutex, Result<BlockAck, SdoError<TX::Error>>>,
 }
 
 enum SdoRequest<'a> {
@@ -79,8 +84,9 @@ enum Pending {
     SegmentedDownloadInit { index: u16, sub: u8 },
     UploadSegment { toggle: bool },
     DownloadSegment { toggle: bool },
-    // FUTURE: A state for block transfers would be added here.
-    // e.g. BlockTransfer { state: BlockState }
+    BlockDownloadInitiate { index: u16, sub: u8 },
+    BlockDownloadAck,
+    BlockDownloadEnd,
 }
 
 #[derive(Copy, Clone)]
@@ -88,6 +94,16 @@ pub struct Segment {
     last: bool,
     len: usize,
     data: [u8; 7],
+}
+
+pub struct BlockInit {
+    blksize: u8,
+    server_supports_crc: bool,
+}
+
+pub struct BlockAck {
+    ackseq: u8,
+    next_blksize: u8,
 }
 
 struct PendingGuard<'a> {
@@ -118,6 +134,8 @@ impl<FRAME: Frame, TX: CanTx<Frame = FRAME>> SdoClient<FRAME, TX> {
                 sig_word: Signal::new(),
                 sig_seg: Signal::new(),
                 sig_ack: Signal::new(),
+                sig_block_init: Signal::new(),
+                sig_block_ack: Signal::new(),
             },
             can_tx: Mutex::new(tx),
             timeout,
@@ -209,15 +227,16 @@ impl<FRAME: Frame, TX: CanTx<Frame = FRAME>> SdoClient<FRAME, TX> {
         self.read_block_locked(index, sub, buf).await
     }
 
-    #[allow(dead_code)]
     pub async fn write_block(
         &self,
         index: u16,
         sub: u8,
         data: &[u8],
+        request_crc_support: bool,
     ) -> Result<(), SdoError<TX::Error>> {
         let _guard = self.request_lock.lock().await;
-        self.write_block_locked(index, sub, data).await
+        self.write_block_locked(index, sub, data, request_crc_support)
+            .await
     }
 
     pub async fn on_frame_received(&self, frame: FRAME) {
@@ -324,6 +343,57 @@ impl<FRAME: Frame, TX: CanTx<Frame = FRAME>> SdoClient<FRAME, TX> {
                     } else {
                         self.state.sig_ack.signal(Err(SdoError::InvalidResponse));
                     }
+                } else {
+                    self.state.sig_ack.signal(Err(SdoError::InvalidResponse));
+                }
+            }
+            Pending::BlockDownloadInitiate { index, sub } => {
+                // Response to initiate block download: cs = 10100r00b
+                if (command & 0b1111_1011) == 0b1010_0000 {
+                    let response_index = u16::from_le_bytes(frame.data()[1..3].try_into().unwrap());
+                    let response_sub = frame.data()[3];
+                    if response_index != index || response_sub != sub {
+                        self.state
+                            .sig_block_init
+                            .signal(Err(SdoError::InvalidResponse));
+                        return;
+                    }
+                    let blksize = frame.data()[4];
+                    let server_supports_crc = (command & 0b0000_0100) != 0;
+
+                    self.state.sig_block_init.signal(Ok(BlockInit {
+                        blksize,
+                        server_supports_crc,
+                    }));
+                } else {
+                    self.state
+                        .sig_block_init
+                        .signal(Err(SdoError::InvalidResponse));
+                }
+            }
+
+            Pending::BlockDownloadAck => {
+                // Response to sub-block: cs = 10100010b
+                if command == 0xA2 {
+                    // 10100010b
+                    let ackseq = frame.data()[1];
+                    let next_blksize = frame.data()[2];
+                    self.state.sig_block_ack.signal(Ok(BlockAck {
+                        ackseq,
+                        next_blksize,
+                    }));
+                } else {
+                    self.state
+                        .sig_block_ack
+                        .signal(Err(SdoError::InvalidResponse));
+                }
+            }
+
+            Pending::BlockDownloadEnd => {
+                // Response to end download: cs = 10100001b
+                if command == 0xA1 {
+                    // 10100001b
+                    self.state.sig_ack.signal(Ok(()));
                 } else {
                     self.state.sig_ack.signal(Err(SdoError::InvalidResponse));
                 }
@@ -442,26 +512,170 @@ impl<FRAME: Frame, TX: CanTx<Frame = FRAME>> SdoClient<FRAME, TX> {
         _sub: u8,
         _buf: &mut [u8],
     ) -> Result<(), SdoError<TX::Error>> {
-        unimplemented!("SDO Block Transfer is not yet implemented");
+        unimplemented!("SDO Block Read is not yet implemented");
     }
 
     async fn write_block_locked(
         &self,
-        _index: u16,
-        _sub: u8,
-        _data: &[u8],
+        index: u16,
+        sub: u8,
+        data: &[u8],
+        request_crc_support: bool,
     ) -> Result<(), SdoError<TX::Error>> {
-        unimplemented!("SDO Block Transfer is not yet implemented");
+        const XMODEM: Crc<u16> = Crc::<u16>::new(&Algorithm {
+            width: 16,
+            poly: 0x1021,
+            init: 0x0000,
+            refin: false,
+            refout: false,
+            xorout: 0x0000,
+            check: 0x31C3,
+            residue: 0x0000,
+        });
+        let mut crc_digest = XMODEM.digest();
+
+        // --- Initiate Block Download ---
+        *self.state.pending.borrow_mut() = Some(Pending::BlockDownloadInitiate { index, sub });
+        let _guard = PendingGuard::new(&self.state.pending);
+        self.state.sig_block_init.reset();
+
+        self.send_initiate_block_download(index, sub, data.len() as u32, request_crc_support)
+            .await
+            .map_err(SdoError::TxError)?;
+
+        let BlockInit {
+            mut blksize,
+            server_supports_crc,
+        } = match embassy_time::with_timeout(self.timeout, self.state.sig_block_init.wait()).await {
+            Ok(res) => res?,
+            Err(_) => return Err(SdoError::Timeout),
+        };
+
+        let use_crc = request_crc_support && server_supports_crc;
+
+        if blksize == 0 || blksize > 127 {
+            return Err(SdoError::SdoAbort(ABORT_INVALID_BLOCK_SIZE));
+        }
+
+        // --- Main Loop: Send sub-blocks ---
+        let mut offset = 0usize;
+
+        while offset < data.len() {
+            // --- Send one sub-block ---
+            let sub_block_start_offset = offset;
+            let mut last_sent_seqno_in_block = 0;
+
+            for seqno in 1..=blksize {
+                let chunk_end = (offset + 7).min(data.len());
+                let chunk = &data[offset..chunk_end];
+
+                if use_crc {
+                    crc_digest.update(chunk);
+                }
+
+                let last_segment_of_transfer = chunk_end == data.len();
+
+                self.send_block_download_segment(chunk, seqno, last_segment_of_transfer)
+                    .await
+                    .map_err(SdoError::TxError)?;
+
+                offset = chunk_end;
+                last_sent_seqno_in_block = seqno;
+
+                if last_segment_of_transfer {
+                    break;
+                }
+            }
+
+            // --- Await sub-block acknowledgement ---
+            'ack_loop: loop {
+                *self.state.pending.borrow_mut() = Some(Pending::BlockDownloadAck);
+                self.state.sig_block_ack.reset();
+
+                let BlockAck {
+                    ackseq,
+                    next_blksize,
+                } = match embassy_time::with_timeout(self.timeout, self.state.sig_block_ack.wait())
+                    .await
+                {
+                    Ok(res) => res?,
+                    Err(_) => return Err(SdoError::Timeout),
+                };
+
+                if ackseq > last_sent_seqno_in_block {
+                    return Err(SdoError::InvalidResponse);
+                }
+
+                if ackseq == last_sent_seqno_in_block {
+                    if next_blksize > 0 && next_blksize <= 127 {
+                        blksize = next_blksize;
+                    } else if next_blksize != 0 {
+                        return Err(SdoError::SdoAbort(ABORT_INVALID_BLOCK_SIZE));
+                    }
+                    break 'ack_loop;
+                } else {
+                    // Retransmission needed
+                    let retransmit_start_offset = sub_block_start_offset + (ackseq as usize * 7);
+                    let mut retransmit_offset = retransmit_start_offset;
+
+                    for i in 0..(last_sent_seqno_in_block - ackseq) {
+                        let retransmit_seqno = ackseq + 1 + i;
+                        let chunk_end = (retransmit_offset + 7).min(data.len());
+                        let chunk = &data[retransmit_offset..chunk_end];
+                        let is_last = chunk_end == data.len();
+
+                        self.send_block_download_segment(chunk, retransmit_seqno, is_last)
+                            .await
+                            .map_err(SdoError::TxError)?;
+
+                        retransmit_offset = chunk_end;
+                        if is_last {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // --- End of Transfer ---
+        let crc_val = if use_crc {
+            crc_digest.finalize()
+        } else {
+            0x0000
+        };
+
+        let unused_bytes_in_last_segment = if data.is_empty() {
+            7
+        } else {
+            let last_segment_len = (data.len() - 1) % 7 + 1;
+            (7 - last_segment_len) as u8
+        };
+
+        *self.state.pending.borrow_mut() = Some(Pending::BlockDownloadEnd);
+        self.state.sig_ack.reset();
+
+        self.send_end_block_download(unused_bytes_in_last_segment, crc_val)
+            .await
+            .map_err(SdoError::TxError)?;
+
+        match embassy_time::with_timeout(self.timeout, self.state.sig_ack.wait()).await {
+            Ok(res) => res?,
+            Err(_) => return Err(SdoError::Timeout),
+        };
+
+        Ok(())
     }
 
     fn signal_error(&self, pending: Pending, err: SdoError<TX::Error>) {
         match pending {
             Pending::ExpeditedRead { .. } => self.state.sig_word.signal(Err(err)),
-            Pending::ExpeditedWrite { .. } | Pending::SegmentedDownloadInit { .. } => {
-                self.state.sig_ack.signal(Err(err))
-            }
+            Pending::ExpeditedWrite { .. }
+            | Pending::SegmentedDownloadInit { .. }
+            | Pending::DownloadSegment { .. }
+            | Pending::BlockDownloadEnd => self.state.sig_ack.signal(Err(err)),
             Pending::UploadSegment { .. } => self.state.sig_seg.signal(Err(err)),
-            Pending::DownloadSegment { .. } => self.state.sig_ack.signal(Err(err)),
+            Pending::BlockDownloadInitiate { .. } => self.state.sig_block_init.signal(Err(err)),
+            Pending::BlockDownloadAck => self.state.sig_block_ack.signal(Err(err)),
         }
     }
 
@@ -548,6 +762,66 @@ impl<FRAME: Frame, TX: CanTx<Frame = FRAME>> SdoClient<FRAME, TX> {
         let mut payload = [0u8; 8];
         // Upload SDO Segment Request: 011t 0000 b
         payload[0] = 0x60 | if toggle { 0x10 } else { 0x00 };
+        let frame = FRAME::new(Id::Standard(StandardId::new(id).unwrap()), &payload).unwrap();
+        self.can_tx.lock().await.transmit(&frame).await
+    }
+
+    async fn send_initiate_block_download(
+        &self,
+        index: u16,
+        sub: u8,
+        size: u32,
+        crc: bool,
+    ) -> Result<(), TX::Error> {
+        let id = 0x600 + (self.node_id as u16);
+        let mut payload = [0u8; 8];
+        // Initiate block download CS: 11000rs0b
+        let mut cs = 0b1100_0000;
+        if crc {
+            cs |= 0b0000_0100; // r bit (CRC support)
+        }
+        cs |= 0b0000_0010; // s bit (size indicated)
+
+        payload[0] = cs;
+        payload[1..3].copy_from_slice(&index.to_le_bytes());
+        payload[3] = sub;
+        payload[4..8].copy_from_slice(&size.to_le_bytes());
+
+        let frame = FRAME::new(Id::Standard(StandardId::new(id).unwrap()), &payload).unwrap();
+        self.can_tx.lock().await.transmit(&frame).await
+    }
+
+    async fn send_block_download_segment(
+        &self,
+        segment_data: &[u8],
+        seqno: u8,
+        last_segment: bool,
+    ) -> Result<(), TX::Error> {
+        let id = 0x600 + (self.node_id as u16);
+        let mut payload = [0u8; 8];
+
+        // Download block segment CS: cnnnnnnnb
+        let mut cs = seqno;
+        if last_segment {
+            cs |= 0x80; // c bit
+        }
+
+        payload[0] = cs;
+        payload[1..1 + segment_data.len()].copy_from_slice(segment_data);
+
+        let frame = FRAME::new(Id::Standard(StandardId::new(id).unwrap()), &payload).unwrap();
+        self.can_tx.lock().await.transmit(&frame).await
+    }
+
+    async fn send_end_block_download(&self, unused_bytes: u8, crc: u16) -> Result<(), TX::Error> {
+        let id = 0x600 + (self.node_id as u16);
+        let mut payload = [0u8; 8];
+        // End block download CS: 110nnn01b
+        let cs = 0b1100_0001 | (unused_bytes << 2);
+
+        payload[0] = cs;
+        payload[1..3].copy_from_slice(&crc.to_le_bytes());
+
         let frame = FRAME::new(Id::Standard(StandardId::new(id).unwrap()), &payload).unwrap();
         self.can_tx.lock().await.transmit(&frame).await
     }
